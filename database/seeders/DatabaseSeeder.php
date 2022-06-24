10784f229cf02a0cc6dbb9957d418c55a0b714d4
Shaon Majumder <smazoomder@gmail.com>
2022-06-24 13:57:08 +0600

    init

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\InteractionStatus;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $account_seeder = new AccountSeeder();
+        $account_types = $account_seeder->run();
+        $user_seeder = new UserSeeder($account_types);
+        $user_seeder->run();
+    }
+}
